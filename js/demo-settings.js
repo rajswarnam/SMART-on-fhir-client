bc7be22b3bdf0ec2010b51ca816e42f93ec525ba
Sanatt Abrol <sanatt.abrol.in@gmail.com>
2017-08-14 19:37:32 +0530

    Initial Commit

---
+++ js/demo-settings.js
@@ -0,0 +1,11 @@
+var demo = {
+  serviceUrl: "localhost:8080/openmrs/ws/fhir",
+  patientId: "SMART-1137192"
+};
+
+/*
+var demo = {
+  serviceUrl: "http://fhir.healthintersections.com.au/open",
+  patientId: "f001"
+};
+*/
